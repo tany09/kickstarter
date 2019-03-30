dafb2ba7e354585cfebf4659a54817e726231241
Tanveer Singh Dhawan <tanysingh09@gmail.com>
2019-03-30 22:00:54 +0530

    Initial Commit

---
+++ test/campaign.test.js
@@ -0,0 +1,55 @@
+const assert =  require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const compiledFactory = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
+
+let accounts;
+let campaignAddress;
+let campaign;
+let campaignFactory;
+
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+    campaignFactory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({data : compiledFactory.bytecode})
+    .send({from : accounts[0], gas: 1000000});
+
+    await campaignFactory.methods.createCampaign('100').send({from: accounts[0], gas :'1000000'});
+
+    const addresses = await campaignFactory.methods.getDeployedCampaigns().call();
+    campaignAddress = addresses[0];
+
+    campaign = await new web3.eth.Contract(JSON.parse(compiledCampaign.interface), campaignAddress);
+});
+
+describe('campaign', () => {
+    it('deploys a factory and campaign contract', () => {
+        assert.ok(campaignFactory.options.address);
+        assert.ok(campaign.options.address);
+    });
+
+    it('marks caller as the campaign manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
+
+    it('allows people to contribute money and marks them as contributors', async () => {
+        await campaign.methods.contribute().send({from : accounts[1], gas : '1000000', value : web3.utils.toWei('1', 'ether')});
+        const isContributor = await campaign.methods.approvers(accounts[1]).call();
+        assert(isContributor);
+    });
+
+    it('requires a minimum contribution', async () => {
+        try {
+            await campaign.methods.contribute().send({from: accounts[1], value: web3.utils.toWei('0', 'ether'), gas: '1000000'});
+            assert(false);
+        } catch(err) {
+            const isContributor = await campaign.methods.approvers(accounts[1]).call();
+            assert(!isContributor);
+        }
+    });
+})
