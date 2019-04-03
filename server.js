000429435de3d7e5b1b6b287fda3eb73984ff2f1
Tanveer Singh Dhawan <tanysingh09@gmail.com>
2019-04-03 21:09:18 +0530

    edit files for deployment (server.js and package.json)

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const next = require('next');
 
 const routes = require('./routes');
-const app = next({dev:  process.env.NODE_ENV !== 'production'});
+const app = next({dev});
 const handler = routes.getRequestHandler(app);
+const port = process.env.PORT
 
 const {createServer} = require('http');
 app.prepare().then(() => {
-    createServer(handler).listen(3000, (err) => {
+    createServer(handler).listen(port, (err) => {
         if (err) throw err;
         console.log('Ready on localhost 3000');
     });
